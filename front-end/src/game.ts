8d124deb746a3827b4366a88e044ae5a3a239221
Malavika Kilaru <malavikakrishnann0208@gmail.com>
2025-11-23 13:23:34 -0600

    Assignemnt 4:TicTacToe with instructions,undo,and dark theme

---
+++ front-end/src/game.ts
@@ -0,0 +1,14 @@
+interface GameState {
+  cells: Cell[];
+  currentPlayer?:string;
+  winner?:string|null;
+}
+
+interface Cell {
+  text: string;
+  playable: boolean;
+  x: number;
+  y: number;
+}
+
+export type { GameState, Cell }
